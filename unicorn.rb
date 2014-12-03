1e4039b5209ddaa11b7b0cf5c7df5940df7fcd5c
Noah Paessel <knowuh@gmail.com>
2014-12-03 16:19:16 -0500

    Configure unicorn worker count by ENV.

--- unicorn.rb
+++ unicorn.rb
@@ -1,2 +1,6 @@
-worker_processes 4
-        timeout 30
+default_workers = 4
+env_worker_param = ENV['SB_WORKERS'].to_i
+number_of_workers = env_worker_param < default_workers ? default_workers : env_worker_param
+
+worker_processes number_of_workers
+        timeout 90
